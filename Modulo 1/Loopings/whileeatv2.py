c090877c555e0f960842696e6f97c7f75fbbfdce
vitoria <vm4650894@gmail.com>
2025-09-10 14:17:46 -0300

    Add files via upload

---
+++ Modulo 1/Loopings/whileeatv2.py
@@ -0,0 +1,8 @@
+nota= int(input('digite uma nota'))
+
+while nota < 0 or nota>10:
+    print('digite um valor valido so pode de 0 a 10')
+    nota = int (input('valor invalido, digite novamente'))      
+
+print('valor valido')
+
